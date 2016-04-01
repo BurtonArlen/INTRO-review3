2bb3fbcdd8e040dc27ceeecf18bb197beb013355
Arlen Burton <burton.arlen.1990@gmail.com>
2016-04-01 10:52:01 -0700

    appear more complete to a user

--- js/js.js
+++ js/js.js
@@ -6,17 +6,27 @@ $(document).ready(function() {
 
 
 // business end
-    var firstNumber = parseInt($("input#numberOne").val());
-      console.log(firstNumber);
 
-    if ((isNaN(firstNumber)) || (firstNumber < 0)){
-      $("#outputList").append( "invalid input" );
-    } else {
-      for (var count = firstNumber; count = 1; count--){
+var firstNumber = parseInt($("input#numberOne").val());
+  console.log(firstNumber);
 
-        $("#outputList").append(count + ", ");
-      }
+  for (var i = firstNumber; i >= 1 ; i--) {
+    console.log(i);
+    if ((i%3 === 0) && (i%15 !== 0)) {
+      $("p#outputList").append("PING! ");
     }
+    if ((i%5 === 0) && (i%15 !== 0)) {
+      $("p#outputList").append("PONG !");
+    }
+    if (i%15 === 0) {
+      $("p#outputList").append("PANGPUNG !");
+    }
+    else if ((i%15 !== 0) && (i%5 !== 0) && (i%3 !== 0)) {
+      $("p#outputList").append(i + ", ");
+
+
+    }
+  }
 
   });
 });
