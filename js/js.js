b3097af333716a65a2461c666606012bdcf82fd0
Arlen Burton <burton.arlen.1990@gmail.com>
2016-04-06 19:44:43 -0700

    further separate business and user logics, as well as improve the function to clear the input and array values

--- js/js.js
+++ js/js.js
@@ -1,34 +1,41 @@
-//User reliant function
+// Logic function
+var results = [];
+var finalString = "";
+var firstNumber = parseInt($("input#numberOne").val());
+function checkValid(){
+  var firstNumber = parseInt($("input#numberOne").val());
+  if ((isNaN(firstNumber)) || (firstNumber < 0)) {
+    results.push("INVALID INPUT! PLEASE ENTER A NUMBER GREATER THAN 0!!!!");
+  }
+}
+function pingPongMath(){
+  var firstNumber = parseInt($("input#numberOne").val());
+  for (var i = firstNumber; i >= 1 ; i--) {
+    if ((i%3 === 0) && (i%15 !== 0)) {
+      results.push('PING!');
+    } if ((i%5 === 0) && (i%15 !== 0)) {
+      results.push('PONG!');
+    } if (i%15 === 0) {
+      results.push('PINGPONG!');
+    } if ((i%15 !== 0) && (i%5 !== 0) && (i%3 !== 0)) {
+      results.push(i);
+    }
+  }
+  convertArray();
+}
+function convertArray(){
+  finalString = results.join(", ");
+}
+//User function
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-
-$("button#clear").click(function() {
-  $("p#outputList").empty();
-});
-
-// business end
-
-var firstNumber = parseInt($("input#numberOne").val());
-  console.log(firstNumber);
-
-  for (var i = firstNumber; i >= 1 ; i--) {
-    console.log(i);
-    if ((i%3 === 0) && (i%15 !== 0)) {
-      $("p#outputList").append("PING! ");
-    }
-    if ((i%5 === 0) && (i%15 !== 0)) {
-      $("p#outputList").append("PONG !");
-    }
-    if (i%15 === 0) {
-      $("p#outputList").append("PANGPUNG !");
-    }
-    else if ((i%15 !== 0) && (i%5 !== 0) && (i%3 !== 0)) {
-      $("p#outputList").append(i + ", ");
-    }
-    if ( (isNaN(firstNumber)) || (firstNumber < 0)){
-    alert("THIS INPUT ONLY ACCEPTS NUMBER VALUES GREATER THAN 0");
-  }
-}
+    $("p#outputList").empty();
+    checkValid();
+    pingPongMath();
+    $("input#numberOne").val("");
+    $("p#outputList").append(finalString);
+    results = [];
+    finalString = [];
   });
 });
